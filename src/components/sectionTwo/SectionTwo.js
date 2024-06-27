b615f04b82c0fcf20172f7a8dcfa1538432d2206
github_christiandc19 <christiandc19@hotmail.com>
2024-06-27 00:37:39 -0700

    styling

--- src/components/sectionTwo/SectionTwo.js
+++ src/components/sectionTwo/SectionTwo.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import Gauge from './Gauge'
 import './SectionTwo.css'
+import CountUp from 'react-countup';
 
 
 
 const SectionTwo = () => {
   return (
 <>
-            <div class="sectionTwo">
+        <div class="sectionTwo">
 
             <div className="sectionTwoHeader container">
                 <h1>Over 2200 healthcare professionals placed in 5 years of dedicated service.</h1>
@@ -15,48 +15,62 @@ const SectionTwo = () => {
          
 
             <div className="container about-skills-container">
-
               <div className='sectionTwoFlex'>
+
                 <div className='sectionTwoFlexItem'>
-                  <Gauge
-                      Gauge="5"
-                      Name="years of healthcare recruiting experience"
-                      ClassName="recruiting_experience"
-                  />
+                    <h1>
+                      <CountUp
+                        start={0}
+                        end={5}
+                        duration={10}
+                        suffix=""/>
+                    </h1>
+                    <h2>Years of healthcare recruiting experience</h2>
+
                 </div>
 
                 <div className='sectionTwoFlexItem'>
-                  <Gauge
-                      Gauge="2,250"
-                      Name="new employees placed at healthcare organizations in 2023"
-                      ClassName="new_employees"
-                  />
+                    <h1>
+                      <CountUp
+                        start={0}
+                        end={2250}
+                        duration={10}
+                        suffix=""/>
+                    </h1>
+                    <h2>New employees placed at healthcare organizations in 2023</h2>
+                </div>
+
+                <div className='sectionTwoFlexItem'>
+                    <h1>
+                      <CountUp
+                        start={0}
+                        end={35}
+                        duration={10}
+                        suffix=""/>
+                    </h1>
+                    <h2>Days time-to-fill for RPO engagements</h2>
+
+                </div>
+
+                <div className='sectionTwoFlexItem'>
+                    <h1>
+                      <CountUp
+                        start={0}
+                        end={96}
+                        duration={10}
+                        suffix="%"/>
+                    </h1>
+                    <h2>Satisfied clients</h2>
+
                 </div>
 
 
-                <div className='sectionTwoFlexItem'>
-                <Gauge
-                    Gauge="35"
-                    Name="days time-to-fill for RPO engagements"
-                    ClassName="days" />
-
-              </div>
-
-
-              <div className='sectionTwoFlexItem'>
-                <Gauge
-                    Gauge="96%"
-                    Name="satisfied clients"
-                    ClassName="satisfied_clients" />
-              </div>
 
             </div>
-
-
-
-            </div>
-            </div>
-            </>  )
+          </div>
+        </div>
+      </>  
+      )
 }
 
 export default SectionTwo
